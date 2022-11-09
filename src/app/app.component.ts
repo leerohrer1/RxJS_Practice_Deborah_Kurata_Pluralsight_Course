2f62aa65a5d9e28278c94541234e9ca1b3148c00
leerohrer1 <99694297+leerohrer1@users.noreply.github.com>
2022-11-09 16:07:46 -0600

    intro demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    of(2,4,6,8).subscribe(item => console.log(item))
+
+    from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log(`resulting item... ${item}`),
+      error: (err) => console.log(`error occured ${err}`),
+      complete: () => console.log(`complete`)
+    })
+
+    from(['first', 'second', 'third']).subscribe({
+      next: (order) => console.log(`${order} resulting item`),
+      error: (err) => console.log(`error occured ${err}`),
+      complete: () => console.log(`no more items`)
+    })
+  }
 }
